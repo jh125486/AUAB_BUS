dc5751cf99480c4bdc8bc970a7a767dcf3727bda
Jacob Hochstetler <jacob.hochstetler@gmail.com>
2014-02-19 17:43:01 +0300

    added pod stuff

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ConciseKit
+#define COCOAPODS_POD_AVAILABLE_ConciseKit
+#define COCOAPODS_VERSION_MAJOR_ConciseKit 0
+#define COCOAPODS_VERSION_MINOR_ConciseKit 1
+#define COCOAPODS_VERSION_PATCH_ConciseKit 2
+
+// QuickDialog
+#define COCOAPODS_POD_AVAILABLE_QuickDialog
+#define COCOAPODS_VERSION_MAJOR_QuickDialog 0
+#define COCOAPODS_VERSION_MINOR_QuickDialog 9
+#define COCOAPODS_VERSION_PATCH_QuickDialog 1
+
